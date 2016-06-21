9abf66a53c40de609e3670731b6aac860837a18c
ZenBarrier <anthony.j.barrera@gmail.com>
2016-06-20 23:37:23 -0400

    Player Controller
    Script to move player left and right.

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+    static MusicPlayer instance = null;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            GameObject.Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+    
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        } else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+        }
+	}
+}
