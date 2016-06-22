966ff4e9a39378ec8b43b185670d9b9d9188096b
ZenBarrier <anthony.j.barrera@gmail.com>
2016-06-22 00:29:21 -0400

    Enemy Movement
    enemy formation moves left and right within bounds.

--- Assets/Entities/EnemyFormation/EnemySpawner.cs
+++ Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class EnemySpawner : MonoBehaviour {
 
     public GameObject enemyPrefab;
+    public float width = 10f;
+    public float height = 5f;
+    public float speed = 2f;
+
+    Vector3 direction;
+    private float minX, maxX;
 
 	// Use this for initialization
 	void Start ()
@@ -13,10 +19,26 @@ public class EnemySpawner : MonoBehaviour {
             GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
             enemy.transform.parent = child.transform;
         }
-	}
+        Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,0));
+        Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0));
+        minX = leftMost.x + width/2;
+        maxX = rightMost.x - width/2;
+        direction = new Vector3(-1, 0, 0);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(this.transform.position, new Vector3(width, height));
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        transform.position += direction * Time.deltaTime * speed;
+        float bounded = Mathf.Clamp(transform.position.x, minX, maxX);
+        transform.position = new Vector2(bounded,transform.position.y);
+        if(minX == bounded || maxX == bounded)
+        {
+            direction *= -1;
+        }
 	}
 }
