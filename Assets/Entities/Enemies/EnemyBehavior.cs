30e4bc59bc547d7df5f7883ccde5e2e494e45bfc
ZenBarrier <anthony.j.barrera@gmail.com>
2016-06-22 13:36:29 -0400

    Enemy Takes Damage
    added script for enemy behavior to destroy enemies on damage.

---
+++ Assets/Entities/Enemies/EnemyBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehavior : MonoBehaviour {
+
+    public float health = 150f;
+
+	void OnTriggerEnter2D(Collider2D col)
+    {
+        Projectile missile = col.GetComponent<Projectile>();
+        if (missile)
+        {
+            health -= missile.GetDamage();
+            missile.hit();
+            if (health <= 0f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Entities/Player/Projectile.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+    public float damage = 100f;
+
+    public void hit()
+    {
+        Destroy(gameObject);
+    }
+
+    public float GetDamage()
+    {
+        return damage;
+    }
+}
