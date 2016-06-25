a1bc0547c4adfa9e666a8d3e0000b76b8fc88c7f
ZenBarrier <anthony.j.barrera@gmail.com>
2016-06-25 16:57:35 -0400

    Dynamic Bound Update
    enemy bounds change to make sure edge enemies hit bounds of play space.

--- Assets/Entities/EnemyFormation/EnemySpawner.cs
+++ Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -11,16 +11,61 @@ public class EnemySpawner : MonoBehaviour {
 
     Vector3 direction;
     private float minX, maxX;
+    private float leftEnemyWidth, rightEnemyWidth;
+    private float leftMost, rightMost;
+    private bool move = false;
 
 	// Use this for initialization
 	void Start ()
     {
         SpawnEnemies();
-        Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,0));
-        Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0));
-        minX = leftMost.x + width/2;
-        maxX = rightMost.x - width/2;
+        move = true;
         direction = new Vector3(-1, 0, 0);
+        leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+        rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+        GetBounds();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (move)
+        {
+            MoveFormation();
+        }
+        if (IsFormationEmpty())
+        {
+            Debug.Log("all dead");
+            SpawnUntilFull();
+        }
+        else
+        {
+            GetBounds();
+        }
+    }
+
+    void GetBounds()
+    {
+        minX = 100f;
+        maxX = -minX;
+        foreach (Transform child in transform)
+        {
+            if(child.childCount > 0)
+            {
+                if(minX > child.transform.localPosition.x)
+                {
+                    minX = child.transform.localPosition.x;
+                    leftEnemyWidth = child.GetComponentInChildren<BoxCollider2D>().size.x;
+                }
+                if (maxX < child.transform.localPosition.x)
+                {
+                    maxX = child.transform.localPosition.x;
+                    rightEnemyWidth = child.GetComponentInChildren<BoxCollider2D>().size.x;
+                }
+            }
+        }
+        minX = leftMost - minX + leftEnemyWidth/2;
+        maxX = rightMost - maxX - rightEnemyWidth/2;
     }
 
     void SpawnEnemies()
@@ -39,10 +84,15 @@ public class EnemySpawner : MonoBehaviour {
         {
             GameObject enemy = Instantiate(enemyPrefab, nextFreePosition.position, Quaternion.identity) as GameObject;
             enemy.transform.parent = nextFreePosition.transform;
+            
         }
         if (nextFreePosition)
         {
             Invoke("SpawnUntilFull", spawnDelay);
+        }
+        else
+        {
+            move = true;
         }
     }
 
@@ -51,16 +101,7 @@ public class EnemySpawner : MonoBehaviour {
         Gizmos.DrawWireCube(this.transform.position, new Vector3(width, height));
     }
 	
-	// Update is called once per frame
-	void Update ()
-    {
-        MoveFormation();
-        if (IsFormationEmpty())
-        {
-            Debug.Log("all dead");
-            SpawnUntilFull();
-        }
-	}
+	
 
     void MoveFormation()
     {
@@ -94,6 +135,8 @@ public class EnemySpawner : MonoBehaviour {
                 return false;
             }
         }
+        move = false;
+        transform.position = new Vector2(0, transform.position.y);
         return true;
     }
 }
